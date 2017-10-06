9af63391d4f07a4b2c4c2a40001bc02ac46d5141
Rex Wei <wytings@163.com>
2017-10-06 21:41:30 +0800

    commit base structure

---
+++ route-annotation/src/main/java/com/wytings/annotation/MyClass.java
@@ -0,0 +1,4 @@
+package com.wytings.annotation;
+
+public class MyClass {
+}
---
+++ route-api/src/main/java/com/wytings/route/api/MyClass.java
@@ -0,0 +1,8 @@
+package com.wytings.route.api;
+
+/**
+ * Created by rex on 06/10/2017.
+ */
+
+public class MyClass {
+}
